1a53430e3374efb3cfff99803013860599904245
adawolfs <adawolfs@gmail.com>
2022-09-04 13:12:32 -0600

    Fix indent

--- servers/go/main.go
+++ servers/go/main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Language", "Go")
+    w.Header().Set("Language", "Go")
     fmt.Fprintf(w, "Hola desde Go!")
 }
 
 func main() {
-	fmt.Println("Starting server on port 8080")
+    fmt.Println("Starting server on port 8080")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
