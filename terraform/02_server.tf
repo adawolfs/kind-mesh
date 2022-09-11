af38f1b23b89cde96faa77d9fd053dadaf52044d
adawolfs <adawolfs@gmail.com>
2022-09-10 18:30:29 -0600

    Update terraform module

--- terraform/02_server.tf
+++ terraform/02_server.tf
@@ -13,19 +13,10 @@ resource "digitalocean_droplet" "server" {
   ssh_keys  = ["${digitalocean_ssh_key.kind-mesh.fingerprint}"]
   user_data = data.template_file.user_data.rendered
 
-  provisioner "file" {
-    source      = "../kind/cluster.yaml"
-    destination = "/root/kind-cluster.yaml"
-  }
-  provisioner "file" {
-    source      = "../metallb/configmap.yaml"
-    destination = "/root/metallb-configmap.yaml"
-  }
-
   provisioner "remote-exec" {
     inline = [
       "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
-      "yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin firewalld",
+      "yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin firewalld git",
       "systemctl --now enable firewalld",
       "systemctl --now enable docker",
       "curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64",
@@ -33,10 +24,11 @@ resource "digitalocean_droplet" "server" {
       "sudo mv ./kind /usr/local/sbin/kind",
       "curl -LO \"https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\"",
       "install -o root -g root -m 0755 kubectl /usr/local/sbin/kubectl",
-      "kind create cluster --config /root/kind-cluster.yaml",
+      "git clone https://github.com/adawolfs/kind-mesh.git /root/kind-mesh",
+      "kind create cluster --config /root/kind-mesh/kind/kind-cluster.yaml",
       "kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml",
       "kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml",
-      "kubectl apply -f /root/metallb-configmap.yaml",
+      "kubectl apply -f /root/kind-mesh/metallb/configmap.yaml",
     ]
   }
 
--- terraform/README.md
+++ terraform/README.md
@@ -7,18 +7,21 @@ Terraform is a tool for building, configuring, and deploying infrastructure.
 This configuration creates a DigitalOcean droplet with a Kubernetes cluster and a MetalLB mesh.
 
 ## Installation
+### DigitalOcean Access token
 Create a new access token for the DigitalOcean API: https://docs.digitalocean.com/reference/api/create-personal-access-token/
 
+### Create SSH Key
+
+```bash
+$ ssh-keygen -t ed25519 -C "kind-mesh@digitalocean.com" -q -N '' -f ssh-key
+```
+
+### Inicializar terraform
 ```
 $ docker run -it --rm -v $(pwd):/mesh-kind  -w /mesh-kind --entrypoint sh hashicorp/terraform
 /mesh-kind/terraform # cd terraform
 /mesh-kind/terraform # export TF_VAR_digitalocean_token="dop_v1_...."
 /mesh-kind/terraform # terraform apply
-```
-
-## Destroy
-```
-/mesh-kind/terraform # terraform destroy
 ```
 
 ## Steps
@@ -32,11 +35,6 @@ $ docker run -it --rm -v $(pwd):/mesh-kind  -w /mesh-kind --entrypoint sh hashic
 * Install metallb
 * Create a metallb configmap
 
-## Create SSH Key
-
-```
-ssh-keygen -t ed25519 -C "your_email@example.com"
-```
 
 ## Resources
 ### DigitalOcean sizes
@@ -48,4 +46,12 @@ curl -X GET \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $TF_VAR_digitalocean_token" \
   "https://api.digitalocean.com/v2/sizes" | jq . | less
+```
+
+
+# Don't forget to destroy when you finish
+
+## Destroy
+```
+/mesh-kind/terraform # terraform destroy
 ```
